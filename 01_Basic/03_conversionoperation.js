7bce628ddfd68cf652130c6ffed87e2364d9887b
Venisha123-nakrani <venishanakrani@gmail.com>
2025-01-21 13:50:07 +0530

    Update 03_conversionoperation.js

--- 01_Basic/03_conversionoperation.js
+++ 01_Basic/03_conversionoperation.js
@@ -38,9 +38,9 @@ console.log(nenValue);
 // console.log(2%2);
 
 let str1 = "hello"
-let str2 = "venisha"
+let str4 = "venisha"
 
-let str3 = str1 + str2
+let str3 = str1 + str4
 console.log(str3);
 
 console.log("1"+2);
